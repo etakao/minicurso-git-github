d2ed39fe523ac9888bcd225c56be942dbf94702f
Erick <ericktakao28@gmail.com>
2023-09-25 17:41:09 -0300

    Adiciona operacoes multiplicacao e divisao

--- main.js
+++ main.js
@@ -24,3 +24,25 @@ function raizQuadrada() {
 
   document.getElementById('resultado').innerHTML = resultado.toFixed(2);
 }
+
+function multiplicar() {
+  const input1 = document.getElementById('operando1').value;
+  const input2 = document.getElementById('operando2').value;
+
+  const resultado = parseInt(input1) * parseInt(input2);
+
+  document.getElementById('resultado').innerHTML = resultado.toFixed(2);
+}
+
+function dividir() {
+  const input1 = document.getElementById('operando1').value;
+  const input2 = document.getElementById('operando2').value;
+
+  if (input2 === 0) {
+    return;
+  }
+
+  const resultado = parseInt(input1) / parseInt(input2);
+
+  document.getElementById('resultado').innerHTML = resultado.toFixed(2);
+}
