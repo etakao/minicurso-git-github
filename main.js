32c363d0cef69a9ec4199746012184313642b8b7
Erick <ericktakao28@gmail.com>
2023-09-25 17:32:21 -0300

    Adiciona operacoes soma, subtracao e raiz

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-console.log('Olá Git');
+function somar() {
+  const input1 = document.getElementById('operando1').value;
+  const input2 = document.getElementById('operando2').value;
+
+  const resultado = parseInt(input1) + parseInt(input2);
+
+  document.getElementById('resultado').innerHTML = resultado;
+}
+
+function subtrair() {
+  const input1 = document.getElementById('operando1').value;
+  const input2 = document.getElementById('operando2').value;
+
+  const resultado = parseInt(input1) - parseInt(input2);
+
+  document.getElementById('resultado').innerHTML = resultado;
+}
+
+function raizQuadrada() {
+  const input1 = document.getElementById('operando1').value;
+  const input2 = document.getElementById('operando2').value;
+
+  const resultado = Math.sqrt(parseInt(input1));
+
+  document.getElementById('resultado').innerHTML = resultado.toFixed(2);
+}
